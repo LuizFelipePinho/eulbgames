d190b77a57e96dbb5a3525061320b73cad9ecd9d
LuizFelipePinho <luizfelipesilva458@gmail.com>
2021-11-23 22:59:50 -0300

    add get the login data

--- src/Components/LoginElem/Login.js
+++ src/Components/LoginElem/Login.js
@@ -1,23 +1,41 @@
 import './Login.css'
 import BoxSignUp from './boxsignup.png'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const LoginElem = () => {
+    
+    const [name, setName ] = useState('')
+    const [password, setPassword ] = useState('')
+
+
+    const pegaDados = (event) => {
+        event.preventDefault()
+        console.log('nome: ' + name + ' senha: ' + password );
+
+    }
+
+    const capturaDado = (event) => {
+      
+        event.target.name === 'name' ? setName(event.target.value) : setPassword(event.target.value)  
+
+    }
+
     return (
         <div className="containerLogin">
             <div className="screenLogin">
 
-                <form className="boxForm">
+                <form className="boxForm" onSubmit={pegaDados}>
                     <h4 className="Titlelogin">Login</h4>
                     <label className="loginName" for="name">
                         Nome:
                     </label>
-                    <input className="loginInputName" type="text" name="name" />
+                    <input className="loginInputName" type="text" name="name" onChange={capturaDado}/>
 
                     <label className="loginPassword" for="password">
                         Senha:
                     </label>
-                    <input className="loginInputPassword" type="text" name="password" />
+                    <input className="loginInputPassword" type="text" name="password" onChange={capturaDado}/>
 
                     
                     <button className="btnLogin" type="submit">
