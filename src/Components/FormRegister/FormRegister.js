9e72c335db58418f7af2b02de01be92f1ef324cf
LuizFelipePinho <luizfelipesilva458@gmail.com>
2021-11-24 23:25:25 -0300

    capturing registration data

--- src/Components/FormRegister/FormRegister.js
+++ src/Components/FormRegister/FormRegister.js
@@ -1,34 +1,99 @@
-import './Register.css'
+import "./Register.css";
+import { useState } from "react";
 
 const FormRegister = () => {
-    return(
-        <>
-        <div className="containerFormCreate">
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [nickName, setNickName] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfir, setPasswordConfir] = useState("");
 
-            <div className="boxFormCreate">
-                <h3 className="titleCreate">CADASTRE-SE</h3>
 
-                <form className="FormCreate">
-                     <label className="nameCreate" for="nameCreate">Nome:</label>
-                     <input className="inpNameCrete" type="text" id="nameCreate"></input>     
+  const pegaDados = (event) => {
+    event.preventDefault();
+    const formData = {
+        name: name,
+        email: email,
+        nickname: nickName,
+        password: password,
+        passwordConfirmation: passwordConfir
+    }
+    console.log(formData);
 
-                    <label className="emailCreate" for="emailCreate">Email: </label>
-                    <input className="inpCreateEmail" type="text"></input>           
+};
 
-                    <label className="passCreate" for="passCreate">Senha</label>
-                    <input className="inpCreatePass" type="text" id="passCreate"></input>
 
-                    <button className="btnCriarConta" type="submit"> <span>Criar conta</span></button>
-                         
-                </form>
+  return (
+    <>
+      <div className="containerFormCreate">
+        <div className="boxFormCreate">
+          <h3 className="titleCreate">CADASTRE-SE</h3>
 
-            </div>
+          <form className="FormCreate" onSubmit={pegaDados}>
+            <label className="nameCreate" for="nameCreate">
+              Nome:
+            </label>
+            <input
+              className="inpNameCrete"
+              type="text"
+              id="nameCreate"
+              onChange={event => setName(event.target.value)}
+              required
+            ></input>
+
+            <label className="emailCreate" for="emailCreate">
+              Email:{" "}
+            </label>
+            <input
+              className="inpCreateEmail"
+              type="text"
+              id="emailCreate"
+              onChange={event => setEmail(event.target.value)}
+              required
+            ></input>
+
+            <label className="nickNameCreate" for="nickNameCreate">
+              Apelido:{" "}
+            </label>
+            <input
+              className="inpCreateNickName"
+              type="text"
+              id="nickNameCreate"
+              onChange={event => setNickName(event.target.value)}
+              required
+            ></input>
+
+            <label className="passCreate" for="passCreate">
+              Senha
+            </label>
+            <input
+              className="inpCreatePass"
+              type="text"
+              id="passCreate"
+              onChange={event => setPassword(event.target.value)}
+              required
+            ></input>
+
+            <label className="passCreate" for="passCreate">
+              Confirmação de Senha
+            </label>
+            <input
+              className="inpCreatePass"
+              type="text"
+              id="passCreate"
+              onChange={event => setPasswordConfir(event.target.value)}
+              required
+            ></input>
+
+            <button className="btnCriarConta" type="submit">
+              {" "}
+              <span>Criar conta</span>
+            </button>
+          </form>
         </div>
-       
-        
-        </>
+      </div>
+    </>
+  );
+};
 
-    )
-}
-
-export default FormRegister
+export default FormRegister;
