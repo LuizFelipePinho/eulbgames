5180f58be8a217c11d78c105f092b65bd5c8a5ac
LuizFelipePinho <luizfelipesilva458@gmail.com>
2021-11-25 00:16:09 -0300

    add carousel slider

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,11 +1,23 @@
 import './Slider.css';
 import capaCs from './capa-csgo.jpg'
+import spot from './img1.jpg';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
+import { Carousel } from 'react-responsive-carousel';
+
 
 const Slider = () => {
     return (
         <>
         <section className="slider">
-            <img src={capaCs} alt=""/>    
+            <Carousel>
+                 <img src={capaCs} alt=""/>    
+
+
+                <img src={spot} alt=""/>    
+
+            
+            </Carousel>
         
         </section>
         </>
